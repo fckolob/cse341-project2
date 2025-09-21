aa6722f135f2aa8c8a98311d2795a464d82ccb24
fckolob <131497920+fckolob@users.noreply.github.com>
2025-09-20 21:12:34 -0300

    validation improved

--- validation/validationAnimalProduct.js
+++ validation/validationAnimalProduct.js
@@ -2,19 +2,19 @@ const { body, validationResult } = require('express-validator')
 const productValidationRules = () => {
   return [
     // name must be alphanumeric, must have at least three characters
-    body('name').trim().notEmpty().matches(/^[\w\s.,'-]+$/).isLength({ min: 3}),
+    body('name').trim(' ').notEmpty().matches(/^[\w\s.,'-]+$/).isLength({ min: 3}).withMessage('name must be alphanumeric, must have at least three characters'),
     // brand must be alphanumeric, must have at least three characters
-    body('brand').trim().notEmpty().matches(/^[\w\s.,'-]+$/).isLength( { min: 3 } ),
+    body('brand').trim(' ').notEmpty().matches(/^[\w\s.,'-]+$/).isLength( { min: 3 } ).withMessage('brand must be alphanumeric, must have at least three characters'),
     // description must be alphanumeric, must have at least three characters
-    body('description').trim().notEmpty().matches(/^[\w\s.,'-]+$/).isLength( { min: 3 } ),
+    body('description').trim(' ').notEmpty().matches(/^[\w\s.,'-]+$/).isLength( { min: 3 } ).withMessage('description must be alphanumeric, must have at least three characters'),
     // price must be a float and be at least 0.1
-    body('price').notEmpty().isFloat( { min: 0.1} ),
+    body('price').notEmpty().isFloat( { min: 0.1} ).withMessage('price must be a float and be at least 0.1'),
     // color must be alphabetic, must have at least three characters.
-    body('color').trim().notEmpty().matches(/^[\w\s.,'-]+$/).isLength( { min: 3} ),
+    body('color').trim(' ').notEmpty().matches(/^[\w\s.,'-]+$/).isLength( { min: 3} ).withMessage('color must be alphabetic, must have at least three characters.'),
     // inStock can be yes or no
-    body('inStock').trim().notEmpty().toLowerCase().isIn( [ 'yes', 'no'] ),
+    body('inStock').trim(' ').notEmpty().toLowerCase().isIn( [ 'yes', 'no'] ).withMessage('inStock can be yes or no'),
     // size can be small, medium, large, extralarge or unique
-    body('size').toLowerCase().notEmpty().trim().isIn(['small', 'medium', 'large', 'extralarge', 'unique'])
+    body('size').toLowerCase().notEmpty().trim(' ').isIn(['small', 'medium', 'large', 'extralarge', 'unique']).withMessage('size can be small, medium, large, extralarge or unique')
 
   ]
 }
--- validation/validationPet.js
+++ validation/validationPet.js
@@ -2,19 +2,19 @@ const { body, validationResult } = require('express-validator')
 const petValidationRules = () => {
   return [
     // name must be alphabetic.
-    body('name').trim().notEmpty().isAlpha().isLength({ min: 3}),
-    // age most be an integer
-    body('age').trim().notEmpty().isFloat( {min: 0.1} ),
+    body('name').trim(' ').notEmpty().isAlpha().isLength({ min: 3}).withMessage('name must be alphabetic.'),
+    // age most be a float.
+    body('age').trim(' ').notEmpty().isFloat( {min: 0.1} ).withMessage('age most be a float.'),
     // sex must be male or female.
-    body('sex').trim().toLowerCase().notEmpty().isAlpha().isIn( ['male', 'female'] ),
+    body('sex').trim(' ').toLowerCase().notEmpty().isAlpha().isIn( ['male', 'female'] ).withMessage('sex must be male or female.'),
     // vaccines can be updated or not updated.
-    body('vaccines').toLowerCase().notEmpty().isIn(['updated', 'not updated']),
+    body('vaccines').toLowerCase().notEmpty().isIn(['updated', 'not updated']).withMessage('vaccines can be updated or not updated.'),
     // hair color must be alphabetic, must have at least three characters.
-    body('hairColor').trim().notEmpty().isAlpha().isLength( { min: 3} ),
+    body('hairColor').trim(' ').notEmpty().isAlpha().isLength( { min: 3} ).withMessage('hair color must be alphabetic, must have at least three characters.'),
     // weight must be a float and has a value of at least 0.1.
-    body('weight').trim().notEmpty().isFloat( { min: 0.1} ),
+    body('weight').trim(' ').notEmpty().isFloat( { min: 0.1} ).withMessage('weight must be a float and has a value of at least 0.1.'),
     // RoutineControll must be updated or not updated.
-    body('routineControll').toLowerCase().notEmpty().isIn(['updated', 'not updated'])
+    body('routineControll').toLowerCase().notEmpty().isIn(['updated', 'not updated']).withMessage('RoutineControll must be updated or not updated.')
 
   ]
 }
